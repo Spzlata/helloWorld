51c986cdfbe1fddd137ea749a20ef374d7f17dc4
Spzlata <107999131+Spzlata@users.noreply.github.com>
2022-07-02 13:32:36 +0300

    Name

---
+++ HelloWorld/HelloWorld5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write username");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Mary")
+{
+    Console.WriteLine("Hi Beautiful");
+}
+else
+{
+    Console.Write("Hi!");
+    Console.WriteLine(username);
+}
